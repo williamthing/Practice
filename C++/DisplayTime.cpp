ee1058b3ba17cd55104999ce3751a998de98c3a3
williamthing <williamthing@gmail.com>
2015-03-30 09:24:50 -0700

    new commit

--- C++/DisplayTime.cpp
+++ C++/DisplayTime.cpp
@@ -11,9 +11,8 @@ using namespace std;
 int main()
 {
 	time_t t = time(0) // get the current time
-	struct tm * now = localtime(&t);
-	cout << now->tm_mday << '-' // day
-		<< end1;
+	cout << now->tm_mday	<< end1;
+
 
 	return 0;
 }
