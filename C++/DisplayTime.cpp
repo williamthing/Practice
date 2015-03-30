dad25753e1f786cf821d6188bc91c0adf4f8a057
williamthing <williamthing@gmail.com>
2015-03-30 09:44:38 -0700

    new commit

--- C++/DisplayTime.cpp
+++ C++/DisplayTime.cpp
@@ -3,16 +3,15 @@
 //
 
 
-#include <ctime>
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-	time_t t = time(0) // get the current time
-	cout << now->tm_mday	<< end1;
-
-
+	time_t now = time(0); // get the current time
+	char* dt = ctime(&now);
+	cout << "The local date and time is: " << dt << endl;
 	return 0;
 }
