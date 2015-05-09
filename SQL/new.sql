13ba0ec949680a9d184857df51d76a3865c8af55
williamthing <williamthing@gmail.com>
2015-05-08 21:38:46 -0700

    no more sql

--- SQL/new.sql
+++
@@ -1,5 +0,0 @@
--- William Thing
-
--- Politics Forum
-CREATE Public(TOPIC VARCHAR(80), VOTE INT, ID INT);
-CREATE USER(ID INT, TotalVotes INT); 
---
+++ SQL/public.sql
@@ -0,0 +1,6 @@
+-- William Thing
+
+-- Politics Forum SQL
+
+CREATE TABLE Public(TOPIC VARCHAR(80), VOTE INT, ID INT);
+CREATE TABLE User(ID INT, TotalVotes INT);
