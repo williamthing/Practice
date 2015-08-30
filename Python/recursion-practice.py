8d8722323c4e84190d3fefab8b7cfa29ce54ae02
williamthing <williamthing@gmail.com>
2015-08-30 03:24:43 -0700

    recursion prac

---
+++ Python/recursion-practice.py
@@ -0,0 +1,58 @@
+#!/bin/python
+
+def reverse_string(string):
+    """ method takes a string input and return the reverse
+    off the given string ex. bird becomes drib
+    """
+    if string == "":
+        return ""
+    else:
+        return reverse_string(string[1:]) + string[0]
+
+def powers(n, exp):
+    """ takes in integer n and exponent exp to return
+    what value n is raised to by the exponent
+    Since returns integer any negative exp will return 0
+    easy fix by changing to return double/float
+    """
+    if exp < 0:
+        exp = exp * -1
+        return (1 / powers(n, exp))
+    if exp == 0:
+        return 1
+    if exp == 1:
+        return n
+    else:
+        return n * powers(n, exp-1)
+
+def factorial(n):
+    """ Takes an integer n and returns the factor of n (n!)
+    Assume that not going to be passed a negative integer
+    """
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+if __name__ == "__main__":
+    # Testing reverse_string method
+    word = "bird"
+    print reverse_string(word)
+
+    # test powers function
+    print powers(2, 2)
+    print powers(100, 0)
+    print powers(8, 3)
+    print powers(2, -1)
+
+    # test factorial
+    print factorial(0)
+    print factorial(1)
+    print factorial(3)
+    print factorial(5)
+    print factorial(10)
+
+    
