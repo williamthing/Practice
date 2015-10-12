5194546503859deba1c19361127f840403ef1083
WILLIAM THING <wthing@u.washington.edu>
2015-10-12 16:30:25 -0700

    java edit

--- Java/MoveZeros.java
+++ Java/MoveZeros.java
@@ -16,6 +16,7 @@ public class MoveZeros {
 
    public static void main(String args[]) {
       int[] nums = {0, 1, 0, 3, 12};
+      System.out.println(Arrays.toString(nums));
       /*
       moveZeroes(nums);
       System.out.println(Arrays.toString(nums));
